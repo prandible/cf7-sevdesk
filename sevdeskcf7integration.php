caf27e83c3e31d7e569fdf8fbaf036649baf0c83
PaulAnderie <p.anderie@prandible.com>
2019-03-29 11:28:32 +0100

    Add files via upload

---
+++ sevdeskcf7integration.php
@@ -0,0 +1,238 @@
+<?php
+
+/*
+  Plugin Name: SevDesk Contact Form 7 Automatisierung
+  Description: Dieses Plugin integriert Contact Form 7 mit deiner SevDesk Buchhaltung. Es überträgt bereits eingegebene Daten automatisch an SevDesk.
+  Version: 1.0
+ */
+
+function getCurl() {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+    curl_setopt($curl, CURLOPT_VERBOSE, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    return $curl;
+}
+
+function onFormSubmit() {
+
+
+    $id = $_POST['_wpcf7'];
+
+    $Masterkurs = get_option('Masterkurs_' . $id);
+    $PreisMaster = get_option('PreisMaster_' . $id);
+    $yourName = get_option('your-name_' . $id);
+    $nachname = get_option('nachname_' . $id);
+    $Firma = get_option('Firma_' . $id);
+    $Adresse = get_option('Adresse_' . $id);
+    $yourEmail = get_option('your-email_' . $id);
+    $Rechnungsadresse = get_option('Rechnungsadresse_' . $id);
+    $Ort = get_option('Ort_' . $id);
+
+
+
+    if (isset($_POST[$yourName]) && isset($_POST[$nachname]) && isset($_POST[$yourEmail])) {
+
+
+
+
+        $first_name = $_POST[$yourName];
+        $last_name = $_POST[$nachname];
+        $company = $_POST[$Firma];
+        $street_name_number = $_POST[$Adresse];
+        $email = $_POST[$yourEmail];
+        $Rechnungsadresse = $_POST[$Rechnungsadresse];
+
+        $code = trim($_POST['Ort']);
+        $code = explode(',', $code);
+        if (count($code) == 1) {
+            $code = $code[0];
+            $code = explode(' ', $code);
+        }
+
+        $zip = $code[0];
+        array_shift($code);
+        $city = implode(' ', $code);
+
+        $zip_code = $zip;
+
+        $field1 = $_POST[$Masterkurs];
+        $field2 = $_POST[$PreisMaster];
+
+
+
+        $api_url = 'https://my.sevdesk.de/api/v1';
+        $api_key = get_option('sevdesk_api_key');
+
+        $curl = getCurl();
+
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json'
+        ));
+
+
+
+        curl_setopt($curl, CURLOPT_URL, $api_url . '/SevUser/?token=' . $api_key);
+        $sevUser = json_decode(curl_exec($curl));
+
+        curl_setopt($curl, CURLOPT_URL, $api_url . '/Contact/?limit=10000&depth=1&token=' . $api_key);
+        $contacts = json_decode(curl_exec($curl));
+
+        if (isset($contacts->status) && $contacts->status == 401) {
+            return;
+        }
+
+
+
+
+
+        $name = $first_name . ' ' . $last_name;
+        $new = true;
+        $id = false;
+
+        foreach ($contacts->objects as $cont) {
+            if (trim($company) && $cont->name == $company) {
+                $new = false;
+                $id = $cont->id;
+                break;
+            } elseif (trim($company) == "" && $cont->name2 == $name) {
+                $new = false;
+                $id = $cont->id;
+                break;
+            }
+        }
+
+
+
+
+        if ($new) {
+            /* get categories */
+            curl_setopt($curl, CURLOPT_URL, $api_url . '/Category/?limit=10000&token=' . $api_key);
+            $categories = json_decode(curl_exec($curl));
+
+
+
+            /* create new contact! */
+
+            $cat = new stdClass();
+            $cat_id = $categories->objects[0]->id;
+            $cat_name = $categories->objects[0]->objectName;
+
+
+            curl_setopt($curl, CURLOPT_POST, true);
+
+            if (trim($company)) {
+                curl_setopt($curl, CURLOPT_URL, $api_url . '/Contact/?status=1000&address[country][code]=de&address[country][code]=germany&name=' . urlencode($company) . '&address[zip]=' . urlencode($zip_code) . '&address[city]=' . urlencode($city) . '&address[street]=' . urlencode($street_name_number) . '&name2=' . urlencode($name) . '&category[id]=' . $cat_id . '&category[objectName]=' . $cat_name . '&token=' . $api_key);
+            } else {
+                curl_setopt($curl, CURLOPT_URL, $api_url . '/Contact/?status=1000&address[country][code]=de&address[country][code]=germany&name2=' . urlencode($company) . '&address[zip]=' . urlencode($zip_code) . '&address[city]=' . urlencode($city) . '&address[street]=' . urlencode($street_name_number) . '&name2=' . urlencode($name) . '&surename=' . urlencode($first_name) . '&familyname=' . urlencode($last_name) . '&category[id]=' . $cat_id . '&category[objectName]=' . $cat_name . '&token=' . $api_key);
+            }
+
+            $person = json_decode(curl_exec($curl));
+
+            $id = $person->objects->id;
+
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_URL, $api_url . '/Contact/' . $id . '/addEmail/?key=1&value=' . $email . '&token=' . $api_key);
+            $ca = json_decode(curl_exec($curl));
+
+
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_URL, $api_url . '/ContactAddress/?contact[objectName]=Contact&contact[id]=' . $id . '&country[id]=1&country[objectName]=StaticCountry&zip=' . urlencode($zip_code) . '&city=' . urlencode($city) . '&street=' . urlencode($street_name_number) . '&token=' . $api_key);
+            $ca = json_decode(curl_exec($curl));
+        }
+
+
+
+
+        if (trim($company)) {
+            $name = $company;
+        } else {
+            $name = $first_name . ' ' . $last_name;
+        }
+
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_URL, $api_url . '/Invoice/'
+                . '?header=' . $name
+                . '&invoiceNumber=IV-' . date('Y_m_d_H_i_s')
+                . '&invoiceType=RE'
+                . '&headText=' . urlencode($Rechnungsadresse)
+                . '&addressName=' . urlencode($street_name_number . "\n" . $zip_code . ' ' . $city)
+                . '&invoiceDate=' . date('Y-m-d', strtotime('+ 7 days')) . 'T' . date('H:i:s')
+                . '&contactPerson[id]=' . $sevUser->objects[0]->id
+                . '&contactPerson[objectName]=SevUser'
+                . '&contact[id]=' . $id . ''
+                . '&contact[objectName]=Contact'
+                . '&discount='
+                . '&discountTime='
+                . '&taxRate=0%'
+                . '&taxText='
+                . '&taxType='
+                . '&status=100'
+                . '&smallSettlement='
+                . '&currency=EUR'
+                . '&token=' . $api_key);
+        $invoice = json_decode(curl_exec($curl));
+
+        $price = explode('€', $field1);
+        if (isset($price[1])) {
+            $price = $price[1];
+            $price = explode(',', $price);
+            $price = str_replace('.', '', trim($price[0]));
+        } else {
+            $price = 0;
+        }
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_URL, $api_url . '/InvoicePos/?invoice[id]=' . $invoice->objects->id . '&invoice[objectName]=Invoice&name=' . urlencode($field1) . '&quantity=1&price=' . $price . '&unity[id]=1&unity[objectName]=Unity&taxRate=&token=' . $api_key);
+        $ca = json_decode(curl_exec($curl));
+
+        $price = explode('€', $field2);
+        if (isset($price[1])) {
+            $price = $price[1];
+            $price = explode(',', $price);
+            $price = str_replace('.', '', trim($price[0]));
+        } else {
+            $price = 0;
+        }
+
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_URL, $api_url . '/InvoicePos/?invoice[id]=' . $invoice->objects->id . '&invoice[objectName]=Invoice&name=' . urlencode($field2) . '&quantity=1&price=' . $price . '&unity[id]=1&unity[objectName]=Unity&taxRate=&token=' . $api_key);
+        $ca = json_decode(curl_exec($curl));
+    }
+}
+
+add_action('wpcf7_mail_sent', 'onFormSubmit', 10, 2);
+
+function companies_metabox($post) {
+    ?>
+
+    <?php
+
+}
+
+class BackendPanel {
+
+    public function adminMenu() {
+
+        add_menu_page('SevDesk Config', 'SevDesk Config', 'manage_options', 'sevdesk_config', array($this, 'settings'));
+    }
+
+    public function settings() {
+        if (isset($_POST['save'])) {
+            foreach ($_POST as $key => $value) {
+                update_option($key, stripslashes($value));
+            }
+        }
+        require_once dirname(__FILE__) . '/tmpl/theme-panel.php';
+    }
+
+}
+
+function prepare_backendpanel_plugin() {
+    $backendPanel = new BackendPanel();
+    add_action('admin_menu', array($backendPanel, 'adminMenu'));
+}
+
+add_action('init', 'prepare_backendpanel_plugin');
---
+++ tmpl/theme-panel.php
@@ -0,0 +1,114 @@
+<?php
+global $wpdb;
+?>
+<div id="config-sevdesk">
+    <?php if (isset($_POST['save'])): ?>
+        <p class="save">Änderungen wurden gespeichert</p>
+    <?php endif ?>
+    <form action="" method="post">
+        <div class="one-segment">
+            <label for="sevdesk_api_key">API Key</label>
+            <input type="text" name="sevdesk_api_key" id="sevdesk_api_key" value="<?php echo get_option('sevdesk_api_key') ?>">
+        </div>
+        <?php
+        $args = array('post_type' => 'wpcf7_contact_form', 'posts_per_page' => -1);
+        $cf7Forms = get_posts( $args );
+        if(!empty($cf7Forms))
+        {
+            /*
+Masterkurs
+PreisMaster
+your-name
+nachname
+Firma
+Adresse
+your-email
+Rechnungsadresse
+             *              */
+        ?>
+        <div class="one-segment">
+            <label>Zuordnung</label>
+        </div>
+        <?php foreach($cf7Forms as $form):
+
+            $Masterkurs = get_option('Masterkurs_'.$form->ID);
+            $PreisMaster = get_option('PreisMaster_'.$form->ID);
+            $yourName = get_option('your-name_'.$form->ID);
+            $nachname = get_option('nachname_'.$form->ID);
+            $Firma = get_option('Firma_'.$form->ID);
+            $Adresse = get_option('Adresse_'.$form->ID);
+            $yourEmail = get_option('your-email_'.$form->ID);
+            $Rechnungsadresse = get_option('Rechnungsadresse_'.$form->ID);
+            $Ort = get_option('Ort_'.$form->ID);
+
+
+            ?>
+        <div class="one-segment-form">
+            <p><b>Forms ID:</b> <?php echo $form->ID ?></p>
+            <p><span>Line Item 1</span> <input name="Masterkurs_<?php echo $form->ID ?>" value="<?php echo $Masterkurs ?>" /></p>
+            <p><span>Line Item 2</span> <input name="PreisMaster_<?php echo $form->ID ?>" value="<?php echo $PreisMaster  ?>" /></p>
+            <p><span>Vorname</span> <input name="your-name_<?php echo $form->ID ?>" value="<?php echo $yourName  ?>" /></p>
+            <p><span>Nachname</span> <input name="nachname_<?php echo $form->ID ?>" value="<?php echo $nachname  ?>" /></p>
+            <p><span>Firma</span> <input name="Firma_<?php echo $form->ID ?>" value="<?php echo  $Firma ?>" /></p>
+            <p><span>Adresse</span> <input name="Adresse_<?php echo $form->ID ?>" value="<?php echo $Adresse  ?>" /></p>
+            <p><span>Ort</span> <input name="Ort_<?php echo $form->ID ?>" value="<?php echo $Ort  ?>" /></p>
+            <p><span>Email-Adresse</span> <input name="your-email_<?php echo $form->ID ?>" value="<?php echo $yourEmail  ?>" /></p>
+            <p><span>Rechnungsadresse</span> <input name="Rechnungsadresse_<?php echo $form->ID ?>" value="<?php echo $Rechnungsadresse  ?>" /></p>
+        </div>
+        <?php endforeach ?>
+        <?php } ?>
+
+
+        <div class="one-segment">
+
+            <input type="submit" class="button primary" name='save' value="save">
+        </div>
+    </form>
+</div>
+
+<style>
+
+    #config-sevdesk
+    {
+        padding-top: 50px;
+    }
+
+    .one-segment
+    {
+        margin-bottom:20px;
+    }
+
+    #sevdesk_api_key
+    {
+        width: 100%;
+        max-width: 350px;
+        margin-top: 10px;
+        display:block;
+    }
+
+    .one-segment label
+    {
+        font-weight: bold;
+        display:block;
+    }
+
+    .one-segment-form
+    {
+        margin-bottom: 30px;
+    }
+
+    .one-segment-form span
+    {
+        display: inline-block;
+        width: 120px;
+    }
+
+    .save
+    {
+        font-size:18px;
+        color: #5c5;
+        font-weight:bold;
+        margin-bottom:20px;
+    }
+
+</style>
